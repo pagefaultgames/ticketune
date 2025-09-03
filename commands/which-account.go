6e4b2424f071f3d8cb530dac6946e4e8aa0a19b0
Adri1 <adrien.grivel@hotmail.fr>
2025-09-03 21:05:01 +0200

    Texts adjustments

--- commands/which-account.go
+++ commands/which-account.go
@@ -14,7 +14,7 @@ import (
 	"github.com/amatsagu/tempest"
 )
 
-var whichAccountCommandDescription = "Ping the user associated with this ticket and ask them which account they would like help with"
+var whichAccountCommandDescription = "Ping and ask the user which account they would like help with"
 
 const whichAccountMessage = "Which account would you like help with?"
 
@@ -23,7 +23,7 @@ var WhichAccountCommand = tempest.Command{
 	Description:         whichAccountCommandDescription,
 	RequiredPermissions: tempest.ADMINISTRATOR_PERMISSION_FLAG,
 	SlashCommandHandler: func(itx *tempest.CommandInteraction) {
-		utils.SayCommandTemplate(itx, whichAccountMessage, "The user has been asked which account.")
+		utils.SayCommandTemplate(itx, whichAccountMessage, "The user has been asked which account they need help with.")
 	},
 	Contexts: []tempest.InteractionContextType{tempest.GUILD_CONTEXT_TYPE},
 }
