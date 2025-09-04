9e387d4457c2615cd8286bae3ccc3fdda64f5707
Lugiad <adrien.grivel@hotmail.fr>
2025-09-05 00:28:44 +0200

    Update commands/ping-spam.go
    Co-authored-by: Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>

--- commands/ping-spam.go
+++ commands/ping-spam.go
@@ -17,7 +17,7 @@ var pingSpamCommandDescription = "Ping the user tell them to stop ping abuse"
 
 var pingSpamMessage = "**Please keep in mind that Helpers are real people volunteering on their free time, so please don't ping them over and over.**\n" +
 	"**When someone is free, they'll reach out to help you, but until then, please be patient and wait until someone gets back to you.**\n\n" +
-	"To be more precise, there is currently a colossal amount of 3 people helping on these tickets, all of them also working on other things for the project and having real life on the side.\n" +
+	"To be more precise, there is currently a colossal amount of 3 people helping on these tickets, all of them also working on other things for the project as well as having real life on the side.\n" +
 	"Please understand that the 20 tickets they get every day take a lot of time to deal with, they might be able to check them only once or twice per day and it can take them more than half an hour.\n" +
 	"They will absolutely help you, **just give them time please**!"
 
