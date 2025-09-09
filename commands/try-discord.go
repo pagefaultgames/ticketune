11c59a776567ca4599d15c41164d26a422132a04
Lugiad <adrien.grivel@hotmail.fr>
2025-09-09 23:13:28 +0200

    fix: typos in try-discord (#6)

--- commands/try-discord.go
+++ commands/try-discord.go
@@ -17,12 +17,12 @@ import (
 var tryDiscordCommandDescription = "Ping and ask the user to attempt to log with Discord"
 
 var tryWithDiscordMessage = "Please try to log in with Discord now, and __let us know here if it works__! " +
-	"Make sure you to use the __same Discord account__ you used to open this ticket!\n\n" +
+	"Make sure to use the __same Discord account__ you used to open this ticket!\n\n" +
 	"Alternatively, you can also try this:\n" +
-	"- Open Discord on your web browser\n" +
-	"- Login with the Discord account you are currently using\n" +
-	"- Open PokéRogue in another tab, while keeping the Discord one open\n" +
-	"- On the login page, click on the Discord button to try to log in with Discord"
+	"1. Open Discord on your web browser\n" +
+	"2. Login with the Discord account you used to open this ticket\n" +
+	"3. Open PokéRogue in another tab, while keeping the Discord one open\n" +
+	"4. On the login page, click on the Discord button to try to log in with Discord"
 
 var TryDiscordCommand = tempest.Command{
 	Name:                "try-discord",
