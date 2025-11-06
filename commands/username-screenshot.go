d946b797393ae3e48d3833662000d882a53ab298
Lugiad <adrien.grivel@hotmail.fr>
2025-11-06 23:16:40 +0100

    Pokémon Summary typo

--- commands/username-screenshot.go
+++ commands/username-screenshot.go
@@ -17,7 +17,7 @@ var usernameScreenshotDescription = "Ping and ask the user to check for any scre
 var usernameScreenshotMessage = "By any chance, maybe you have some screenshot with your username visible, or even a PokéRogue save file (.prsv)?\n" +
 	"In some device, Discord server, DMs, etc...?\n\n" +
 	"__The username can apprear on screenshots taken from:__\n" +
-	"- The first page of a Pokémon Sumamry, as OT\n" +
+	"- The first page of a Pokémon Summary, as OT\n" +
 	"- Game stats screen *(since August 23rd 2025)*\n" +
 	"- Title screen *(since October 31st 2025)*"
 
