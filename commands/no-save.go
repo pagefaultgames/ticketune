2c6a04905ac005813972c1e7bc79645411bf1492
Lugiad <adrien.grivel@hotmail.fr>
2025-11-07 18:10:12 +0100

    Typo correction (#9)
    Typo corrections

--- commands/no-save.go
+++ commands/no-save.go
@@ -19,7 +19,7 @@ var tryDifferentBrowserMessage = "If there is another device or browser you've p
 	"Otherwise, please provide:\n" +
 	"- The username of the account you want to recover\n" +
 	"- To the best of your memory, the date of account creation and the date you played for the last time on this account " +
-	"(played for the last time = when you most recently started any kind of run)" +
+	"(played for the last time = when you most recently started any kind of run)\n" +
 	"- Any information regarding game stats and/or the progress of your Pokédex."
 
 var NoSaveCommmand = tempest.Command{
--- commands/username-screenshot.go
+++ commands/username-screenshot.go
@@ -15,9 +15,9 @@ import (
 
 var usernameScreenshotDescription = "Ping and ask the user to check for any screenshot or .prsv file where their username can appear."
 var usernameScreenshotMessage = "By any chance, maybe you have some screenshot with your username visible, or even a PokéRogue save file (.prsv)?\n" +
-	"In some device, Discord server, DMs, etc...?\n\n" +
+	"In some device, Discord server, DMs, etc.?\n\n" +
 	"__The username can apprear on screenshots taken from:__\n" +
-	"- The first page of a Pokémon Sumamry, as OT\n" +
+	"- The first page of a Pokémon Summary, as OT\n" +
 	"- Game stats screen *(since August 23rd 2025)*\n" +
 	"- Title screen *(since October 31st 2025)*"
 
